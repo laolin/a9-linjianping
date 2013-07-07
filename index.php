29d00e796d9f9ce765f5414fbd0737bc7a6cb434
LaoLin <hi@laolin.com>
2013-07-07 08:45:34 +0800

    test

--- index.php
+++ index.php
@@ -8,3 +8,4 @@ define( 'AROOT' , dirname( __FILE__ ) . DS  );
 //ini_set('include_path', dirname( __FILE__ ) . DS .'_lp' );  
 include_once(AROOT.DS.'..'.DS.'~lazyPHP'.DS.'_lp'.DS. '_lp'.DS .'lp.init.php' );
 /**** lp framework init finished ***/
+//test edit is submod
