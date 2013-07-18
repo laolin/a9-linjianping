097874398e2e17f3fe74bf62c11d62239725c5c0
LaoLin <hi@laolin.com>
2013-07-18 20:55:39 +0800

    data of first page

--- controller/home.class.php
+++ controller/home.class.php
@@ -84,7 +84,7 @@ class homeController extends appController
         }else{//TODO:错误处理
           $str.="\n<b x_onclick='location.href=\"{$item[content][link]}\"' class='span{$item[width]} metbox metbox-{$item[content][color]}'>";
           $str.="\n<h2><a href='{$item[content][link]}'>{$item[content][title]}</a></h2>";
-          $str.="\n<p>{$item[content][text]}</p>";
+          $str.="\n{$item[content][text]}";
           $str.="\n</b>";
         }
       }
--- lib/app.data.php
+++ lib/app.data.php
@@ -2,65 +2,45 @@
 function MetorData() {
 $json_rows='
   { "rows":[
-    { "height":2, "data":[          
-        { "width":4,
-          "content":{"color":"purple", "title":"生活流水帐",
-            "text":"<img src=\"http://files.laolin.com/2013/laolin-family/20130525.laolin.family-200.jpg\"/>",
-            "link":"/lin/?cat=2"
-            }
-        },          
-        { "width":4,
-          "content":{"color":"blue", "title":"结构工程师",
-            "text":"<img src=\"http://files.laolin.com/2013/structural-engineering/21030715-structural.jpg\"/>",
-            "link":"/lin/?cat=1"
-            }
-        },           
-        { "width":4,
-          "content":{"color":"orange", "title":"编程爱好者",
-            "text":"<img src=\"http://files.laolin.com/2013/development/20130715-development.jpg\"/>",
-            "link":"/lin/?cat=3"
-            }
-        }
-    ]},
-    { "height":3, "data":[      
-      { "width":4,
-        "content":{"color":"purple", "title":"生活流水帐",
-          "text":"老林日记",
-          "link":"/lin/?cat=2"
-          }
-      },
-      { "width":4,
+    { "height":4, "data":[          
+      { "width":6,
         "content":{"color":"blue", "title":"个人简历",
-          "text":"林建萍，高级工程师，国家一级注册结构工程师<h2>工作经历</h2>2003.4 至今<br/>  同济大学建筑设计研究院（集团）有限公司 结构工程师<br/>1997.8~2000.8<br/> 福建省莆田市涵江区建设局 公务员<h2>学历</h2>2000.9~2003.3<br/> 同济大学 土木工程学院 结构工程 硕士学习<br/>1993.9~1997.7<br/> 福州大学 土木工程学院 工业与民用建筑 本科学习",
-          "link":"./?a=lin&b=index"}
-      },           
-      { "width":4,
-        "content":{"color":"orange", "title":"编程爱好者",
-          "text":"老林是个编程爱好者",
-          "link":"/lin/?cat=2"
+          "text":"<img src=\"http://files.laolin.com/2013/structural-engineering/linjp-2012.9.3-180x180.jpg\"/><br/>林建萍，高级工程师，国家一级注册结构工程师<h2>工作经历</h2>2003.4 至今<br/>  同济大学建筑设计研究院（集团）有限公司 结构工程师<br/>1997.8~2000.8<br/> 福建省莆田市涵江区建设局 公务员<h2>学历</h2>2000.9~2003.3<br/> 同济大学 土木工程学院 结构工程 硕士学习<br/>1993.9~1997.7<br/> 福州大学 土木工程学院 工业与民用建筑 本科学习",
+          "link":"./?a=lin&b=index"
           }
-      }
-    ]}, 
-    { "height":1, "data":[
-      { "width":2,
-        "content":{"color":"purple", "title":"Photos","text":"Coming soon.","link":"#"}
       },
-      { "width":2,
-        "content":{"color":"purple", "title":"Photos","text":"Coming soon.","link":"#"}
-      },
-      { "width":2,
-        "content":{"color":"blue", "title":"Structure","text":"Coming soon.","link":"#"}
-      },
-      { "width":2,
-        "content":{"color":"blue", "title":"Apis","text":"Coming soon.","link":"#"}
-      },
-      { "width":2,
-        "content":{"color":"orange", "title":"Photos","text":"Coming soon.","link":"#"}
-      },
-      { "width":2,
-        "content":{"color":"orange", "title":"Photos","text":"Coming soon.","link":"#"}
-      }
+      { "width":6, "rows": [  
+        { "height":2, "data":[ 
+          { "width":6,
+            "content":{"color":"green", "title":"结构工程师",
+              "text":"<img src=\"http://files.laolin.com/2013/structural-engineering/21030715-structural.jpg\"/>",
+              "link":"/lin/?cat=70"
+              }
+          },
+          { "width":6,
+            "content":{"color":"green", "title":"团队建设",
+              "text":"<img src=\"http://files.laolin.com/2013/structural-engineering/20130715-team.jpg\"/>",
+              "link":"/lin/?cat=72"
+              }
+          }
+        ]},
+        { "height":2, "data":[
+          { "width":6,
+            "content":{"color":"green", "title":"编程爱好者",
+              "text":"<img src=\"http://files.laolin.com/2013/development/20130715-development.jpg\"/>",
+              "link":"/lin/?cat=3"
+              }
+          },
+          { "width":6,
+            "content":{"color":"magenta", "title":"生活流水帐",
+              "text":"<img src=\"http://files.laolin.com/2013/laolin-family/20130525.laolin.family-200.jpg\"/>",
+              "link":"/lin/?cat=2"
+              }
+          }
+        ]}
+      ]}
     ]}
+    
     
   ]}
 ';
