8755d6fbaa078afadad183b7b3bb12fa85d8e78f
LaoLin <hi@laolin.com>
2013-07-19 22:56:45 +0800

    lazyPhp path changed

--- index.php
+++ index.php
@@ -6,6 +6,6 @@ define( 'DS' , DIRECTORY_SEPARATOR );
 define( 'AROOT' , dirname( __FILE__ ) . DS  );
 
 //ini_set('include_path', dirname( __FILE__ ) . DS .'_lp' );  
-include_once(AROOT.DS.'..'.DS.'~lazyPHP'.DS.'_lp'.DS. '_lp'.DS .'lp.init.php' );
+include_once(AROOT.DS.'..'.DS.'~lazyPHP'.DS. '_lp'.DS .'lp.init.php' );
 /**** lp framework init finished ***/
 //test edit is submod
