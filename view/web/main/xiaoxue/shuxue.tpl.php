f2efa4b88e8da4c4d8b3189463cfc91f4ad5ba39
LaoLin <hi@laolin.com>
2013-09-20 16:06:12 +0800

    UC浏览器竟然keypress不灵

--- view/web/main/xiaoxue/shuxue.tpl.php
+++ view/web/main/xiaoxue/shuxue.tpl.php
@@ -9,7 +9,7 @@
           <input type="number" min="1" max="5" name='sx_time' class="form-control" value='1'>分钟
         </li>
         <li class="list-group-item">
-        <input type="number" min="0" max="100" name='sx_count' class="form-control" value='0'>题（0=不限）<br/>
+        <input type="number" min="0" max="100" name='sx_count' class="form-control" value='15'>题（0=不限）<br/>
         </li>
         <li class="list-group-item">
         <input type="number" min="10" max="1000" name='sx_max' class="form-control" value="<?php echo $ks_n; ?>"> 以内加减法   
@@ -24,8 +24,8 @@
       <span class="working">
         <b id="sx_a">A</b><b id="sx_op">+</b><b id="sx_b">B</b>=
         <input id="sx_ans" name='sx_ans' class="form-control" type="number" min="0" max="1000"
-        data-toggle="tooltip" data-placement='bottom' title='答完按[回车],[空格]或[.]'>
-        <!--button type="button" class="btn btn-primary" onclick='sx_answer()'>确定</button-->
+        >
+        <button type="button" class="btn btn-primary" onclick='sx_answer()'>确定(或按[回车],[空格],[.])</button>
       </span>
         <button type="button" class="btn btn-warning" onclick='sx_next()'>跳过</button>
         <button type="button" class="btn btn-info" onclick='sx_end()' id='shuxue-end'
